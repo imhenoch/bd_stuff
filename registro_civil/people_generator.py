7daddd190a860668d10f5c150d2c18eb64651c2b
imhenoch <isidro.henoch@gmail.com>
2018-08-28 20:56:31 -0500

    Fixes date issues

--- registro_civil/people_generator.py
+++ registro_civil/people_generator.py
@@ -7,9 +7,8 @@ genders = ['H', 'M']
 female_names = []
 male_names = []
 last_names = []
-birthday_limit = int(datetime.strptime("1900/1/1", "%Y/%m/%d").strftime("%s"))
 most_ancient_date = int(datetime.strptime("1950/1/1", "%Y/%m/%d").strftime("%s"))
-most_recent_date = int(datetime.strptime("2000/1/1", "%Y/%m/%d").strftime("%s"))
+most_recent_date = int(datetime.strptime("2018/1/1", "%Y/%m/%d").strftime("%s"))
 people = 0 if len(sys.argv) == 1 else int(sys.argv[1])
 counter = 0
 
@@ -25,6 +24,11 @@ for name in open('last_names.dictionary'):
 
 
 def generate_person(aux_dad_lastname, aux_gender, aux_date, prob):
+    global counter
+
+    if counter >= people:
+        return -1
+
     gender = ""
     if aux_gender == None:
         gender = choice(genders)
@@ -38,8 +42,9 @@ def generate_person(aux_dad_lastname, aux_gender, aux_date, prob):
         dad_lastname = aux_dad_lastname
     mom_lastname = choice(last_names)
     date = generate_date(aux_date)
-    print("%(name)s %(dad_lastname)s %(mom_lastname)s - %(gender)s, %(date)s" %
-    { 'name': name, 'dad_lastname': dad_lastname, 'mom_lastname': mom_lastname, 'gender': gender, 'date': datetime.fromtimestamp(date).strftime('%Y-%m-%d')} )
+    # print("%(name)s %(dad_lastname)s %(mom_lastname)s - %(gender)s, %(date)s" % { 'name': name, 'dad_lastname': dad_lastname, 'mom_lastname': mom_lastname, 'gender': gender, 'date': datetime.fromtimestamp(date).strftime('%Y-%m-%d')} )
+
+    counter += 1
 
     statistic = randint(0, 100)
     if statistic < prob:
@@ -56,16 +61,23 @@ def generate_person(aux_dad_lastname, aux_gender, aux_date, prob):
     return prob
 
 def generate_sibling(dad_lastname, mom_lastname, aux_date):
+    global counter
+
+    if counter >= people:
+        return
+
     gender = choice(genders)
     name = choice(male_names) if gender == 'H' else choice(female_names)
     date = generate_similar_date(aux_date)
-    print("%(name)s %(dad_lastname)s %(mom_lastname)s - %(gender)s, %(date)s" %
-    { 'name': name, 'dad_lastname': dad_lastname, 'mom_lastname': mom_lastname, 'gender': gender, 'date': datetime.fromtimestamp(date).strftime('%Y-%m-%d')} )
+
+    counter += 1
+
+    # print("%(name)s %(dad_lastname)s %(mom_lastname)s - %(gender)s, %(date)s" % { 'name': name, 'dad_lastname': dad_lastname, 'mom_lastname': mom_lastname, 'gender': gender, 'date': datetime.fromtimestamp(date).strftime('%Y-%m-%d')} )
 
 def generate_similar_date(date):
     tmp_date = datetime.fromtimestamp(date).date()
-    final_date = tmp_date.replace(year = tmp_date.year + 10)
-    initial_date = final_date.replace(year = final_date.year - 10)
+    final_date = tmp_date.replace(year = tmp_date.year + 5, month = tmp_date.month, day = 1)
+    initial_date = final_date.replace(year = final_date.year - 5, month = tmp_date.month, day = 1)
     return randint(time.mktime(initial_date.timetuple()), time.mktime(final_date.timetuple()))
 
 def generate_date(date = 0):
@@ -73,10 +85,12 @@ def generate_date(date = 0):
         rnd_date = randint(most_ancient_date, most_recent_date)
     else:
         tmp_date = datetime.fromtimestamp(date).date()
-        final_date = tmp_date.replace(year = tmp_date.year - 20)
-        initial_date = final_date.replace(year = final_date.year - 10)
+        final_date = tmp_date.replace(year = tmp_date.year - 20, month = tmp_date.month, day = 1)
+        initial_date = final_date.replace(year = final_date.year - 10, month = final_date.month, day = 1)
         rnd_date = randint(time.mktime(initial_date.timetuple()), time.mktime(final_date.timetuple()))
     return rnd_date
 
-# for i, number in enumerate(range(people, 0, -1), 1):
-generate_person(None, None, 0, 90)
+while counter < people:
+    # print("----------------------- NEW FAMILY TREE HERE -----------------------")
+    generate_person(None, None, 0, 90)
+    # print(counter)

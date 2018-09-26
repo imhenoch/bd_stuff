50857dc9bbf77c27d59bdf9fe9a7ac98ffdebf47
Isidro Henoch <isidro.henoch@gmail.com>
2018-09-26 00:29:34 -0500

    Adds the python code to normalize the data

---
+++ kwikemart/normalize_data.py
@@ -0,0 +1,25 @@
+import csv
+
+file = open('female_names.dictionary', 'w')
+with open('tmp_females') as f:
+    for line in f:
+        file.write("%s\n" % line.split(' ', 1)[0])
+
+file = open('male_names.dictionary', 'w')
+with open('tmp_males') as f:
+    for line in f:
+        file.write("%s\n" % line.split(' ', 1)[0])
+
+file = open('last_names.dictionary', 'w')
+with open('tmp_last_names') as f:
+    for line in f:
+        file.write("%s\n" % line.split(' ', 1)[0])
+
+file = open('companies.dictionary', 'w')
+with open('tmp_companies') as f:
+    for i, row in enumerate(csv.reader(f, delimiter=',')):
+        if i != 0:
+            try:
+                file.write("%s\n" % row[1])
+            except IndexError:
+                continue

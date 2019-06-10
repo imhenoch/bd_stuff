ac7f439bd864cb3339c4fdc85191e09679253e14
Isidro Henoch <imhenoch@protonmail.com>
2019-06-10 05:50:21 -0500

    Finishes email practice

--- oracle_discounts/src/Main.kt
+++ oracle_discounts/src/Main.kt
@@ -1,3 +1,20 @@
+import java.util.*
+
 fun main(args: Array<String>) {
-    println("Test")
+    val scanner = Scanner(System.`in`)
+    print("Client id: ")
+    val clientId = scanner.nextInt()
+    val favorite = getPreferedProduct(clientId)
+    favorite?.let { f ->
+        val discount = getDiscount(favorite)
+        if (discount == null) {
+            println("There's no available discount for \"${f.product}\", create one")
+            println("\nDiscount details: ")
+            val newDiscount = Discount(0, f.productId, scanner.next().replace("_", " "))
+            createDiscount(newDiscount)
+            sendMail(newDiscount, f)
+        } else {
+            sendMail(discount, f)
+        }
+    }
 }
